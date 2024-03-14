e0f2f87eb73dd04605cc9f498915f73287b2ca34
MattBastos <matthewsbasttos@gmail.com>
2024-03-14 13:16:55 -0300

    feat: Implements getCaptchaImageURL function

--- captchaHandler.js
+++ captchaHandler.js
@@ -1,5 +1,26 @@
 const { createWorker } = require('tesseract.js');
 
+const getCaptchaImageURL = async (page) => {
+  try {
+    await page.waitForSelector('form[method="get"][action="/errors/validateCaptcha"]');
+
+    const src = await page.evaluate(() => {
+      const formElement = document.querySelector('form[method="get"][action="/errors/validateCaptcha"]');
+
+      if (formElement) {
+        const image = formElement.querySelector('img');
+        return image ? image.src : null;
+      } else {
+        return null;
+      }
+    });
+  
+    return src;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const getTextFromImage = async (imageURL) => {
   const worker = await createWorker('eng');
   const image = await worker.recognize(imageURL);
@@ -8,4 +29,7 @@ const getTextFromImage = async (imageURL) => {
   return image.data.text;
 };
 
-module.exports = getTextFromImage;
+module.exports = {
+  getTextFromImage,
+  getCaptchaImageURL
+};
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const puppeteer = require("puppeteer");
+const { getCaptchaImageURL, getTextFromImage } = require("./captchaHandler");
 
 (async () => {
   const ADDRESS = 11001;
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   page.goto('https://www.amazon.com/');
   await page.setViewport({ width: 1080, height: 1024 });
+
+  const imageURL = await getCaptchaImageURL(page);
+  const textImage = await getTextFromImage(imageURL);
+
+  console.log(textImage);
 
   // const changeAddressButton = await page.waitForSelector('span.a-button-inner input[data-action-type="SELECT_LOCATION"]');
   // await page.click(changeAddressButton);
